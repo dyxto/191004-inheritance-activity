5126f31750ae497427a40103db16520e5749d11f
dyxto <54917090+dyxto@users.noreply.github.com>
2019-10-04 23:35:29 -0400

    Edited BankAccount.java
    1) Edited BankAccount.java
    - removed getters and setters for deposit and withdraw attributes
    - class attributes were all put on one line (12)
    - all together everything still works the same as before

--- BankAccount.java
+++ BankAccount.java
@@ -9,19 +9,11 @@ package BankAccount;
 
 public class BankAccount {
 	
-	private double balance;
-	private double deposit;
-	private double withdraw;
+	private double balance, deposit, withdraw;
 	
 	public double getBalance() {return balance;}
 	public void setBalance(double balance) {this.balance = balance;}
-
-	public double getDeposit() {return deposit;}
-	public void setDeposit(double deposit) {this.deposit = deposit;}
-
-	public double getWithdraw() {return withdraw;}
-	public void setWithdraw(double withdraw) {this.withdraw = withdraw;}
-
+	
 	public BankAccount(double balance) {
 		this.balance = balance;
 	}
